8ebcf11c3facf777ca0dc299f3281e7156ea417d
java <java@bigdata>
2017-09-06 16:10:38 +0900

    1. commons logging Ad 2. log4j Ad

--- src/main/java/com/example/TestScala.scala
+++
@@ -1,8 +0,0 @@
-package com.example
-
-object TestScala {
-
-    def main(args: Array[String]): Unit = {
-          println("Scala execution test....")
-    }
-}
---
+++ src/main/scala/com/example/TestScala.scala
@@ -0,0 +1,17 @@
+package com.example
+
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+
+object TestScala {
+
+	var log = LogFactory.getLog(TestScala.getClass.getName)
+	
+    def main(args: Array[String]): Unit = {
+          println("Scala execution test....")
+          
+          log.info("############################")
+          log.info("TestScala Commons logging...")
+          log.info("############################")
+    }
+}
